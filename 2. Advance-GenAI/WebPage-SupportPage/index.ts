2657d7c7ee83c0e25fc3480ceb8f617136411c10
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-04-05 14:20:58 +0530

    feat: initialize project with web scraping utility using axios and cheerio

--- 2. Advance-GenAI/WebPage-SupportPage/index.ts
+++ 2. Advance-GenAI/WebPage-SupportPage/index.ts
@@ -1,2 +1,28 @@
 import axios from "axios";
+import * as cheerio from "cheerio";
 
+async function scrapeWebPage(url: string){
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+    
+    const pageHead = $("head").html();
+    const pageBody = $("body").html();
+
+    const internalLinks: string[] = [];
+    const externalLinks: string[] = [];
+
+    $('a').each((_, element) => {
+        const link = $(element).attr('href')
+        if(link?.startsWith('/')){
+            internalLinks.push(link)
+        }else{
+            externalLinks.push(link!)
+        }
+    });
+
+    return { pageHead, pageBody, internalLinks, externalLinks };
+}
+
+scrapeWebPage("https://zest-delta.vercel.app").then((data) => {
+    console.log(data)
+})
