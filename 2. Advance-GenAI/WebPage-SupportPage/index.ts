836b8624765398cdb3674f7e4d0dea85814fa941
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-04-06 22:11:32 +0530

    feat: initialize web scraper with OpenAI vector embedding pipeline

--- 2. Advance-GenAI/WebPage-SupportPage/index.ts
+++ 2. Advance-GenAI/WebPage-SupportPage/index.ts
@@ -1,5 +1,20 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import OpenAI from "openai";
+import dotenv from "dotenv";
+import { ChromaClient } from "chromadb";
+
+dotenv.config();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY!
+});
+
+const client = new ChromaClient({
+    path: 'http://localhost:8000'
+});
+
+client.heartbeat();
 
 async function scrapeWebPage(url: string){
     const { data } = await axios.get(url);
@@ -21,8 +36,55 @@ async function scrapeWebPage(url: string){
     });
 
     return { pageHead, pageBody, internalLinks, externalLinks };
+};
+
+async function generateVectorEmbedding(
+    { text } : {text: string}
+){
+    const embedding = openai.embeddings.create({
+        model: "text-embedding-3-small",
+        input: text,
+        encoding_format: "float"
+    });
+
+    return (await embedding).data[0]?.embedding!;
+};
+
+async function ingest(url: string){
+    const { 
+        pageHead, pageBody, internalLinks, externalLinks 
+    } = await scrapeWebPage(url);
+
+    const headEmbedding = await generateVectorEmbedding({text: pageHead!});
+
+    const chunks = chunkText(pageBody!);
+    const bodyEmbedding = await Promise.all(
+        chunks.map((chunk) => generateVectorEmbedding({text: chunk}))
+    );
+
+    console.log(headEmbedding);
+    console.log(bodyEmbedding);
+};
+
+function chunkText(text: string, size: number = 1000){
+    const paragraphs = text.split('\n\n');
+    const chunks: string[] = [];
+    let currentChunk = "";
+
+    for(const paragraph of paragraphs){
+        if(currentChunk.length + paragraph.length <= size){
+            currentChunk += paragraph + "\n\n";
+        }else{
+            chunks.push(currentChunk);
+            currentChunk = paragraph + "\n\n";
+        }
+    }
+
+    if(currentChunk.length > 0){
+        chunks.push(currentChunk);
+    }
+
+    return chunks;
 }
 
-scrapeWebPage("https://zest-delta.vercel.app").then((data) => {
-    console.log(data)
-})
+ingest("https://zest-delta.vercel.app");
