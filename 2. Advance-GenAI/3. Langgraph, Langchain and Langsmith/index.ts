f1f5376a9200da2d78c1b93423e8b246c9c660af
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-04-11 23:03:40 +0530

    feat: initialize LangGraph project with arithmetic tools and a state-based workflow using Groq LLM

--- 2. Advance-GenAI/3. Langgraph, Langchain and Langsmith/index.ts
+++ 2. Advance-GenAI/3. Langgraph, Langchain and Langsmith/index.ts
@@ -1,11 +1,17 @@
 import { tool } from '@langchain/core/tools';
 import { z } from 'zod';
 import { ChatGroq } from "@langchain/groq";
+import { MessagesAnnotation, StateGraph } from '@langchain/langgraph';
+import { AIMessage } from '@langchain/core/messages';
+import type { ToolMessage, ToolCall } from '@langchain/core/messages';
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const apiKey = process.env.GROQ_API_KEY;
 
 const llm = new ChatGroq({
-  apiKey,
+  apiKey: apiKey,
   model: "llama-3.3-70b-versatile",
 });
 
@@ -50,3 +56,86 @@ const tools = [multiply, add, divide];
 const toolByName = Object.fromEntries(tools.map((tool) => [tool.name, tool]));
 const llmWithTools = llm.bindTools(tools);
 
+
+async function llmCall(state: typeof MessagesAnnotation.State) {
+    const response = await llmWithTools.invoke([
+        {
+            role: 'system',
+            content: 'You are a helpful assistant.'
+        },
+        ...state.messages
+    ]);
+    return { 
+        messages: [response] 
+    };
+};
+
+async function toolNode(state: typeof MessagesAnnotation.State) {
+    const lastMessage = state.messages.at(-1);
+
+    if (
+        !lastMessage || 
+        !(lastMessage instanceof AIMessage) || 
+        !lastMessage.tool_calls?.length
+    ) {
+        return { messages: [] };
+    }
+
+    const toolMessages = await Promise.all(
+        (lastMessage.tool_calls as ToolCall[]).map(async (toolCall: ToolCall) => {
+            const tool = toolByName[toolCall.name];
+            if (!tool) {
+                return {
+                    role: "tool",
+                    content: `Error: Tool ${toolCall.name} not found.`,
+                    tool_call_id: toolCall.id!,
+                };
+            }
+
+            const result = await tool.invoke(toolCall);
+            return {
+                role: "tool",
+                content: typeof result === "string" ? result : JSON.stringify(result),
+                tool_call_id: toolCall.id!,
+            };
+        })
+    );
+
+    return { messages: toolMessages };
+};
+
+function shouldContinue(state: typeof MessagesAnnotation.State) {
+    const lastMessage = state.messages.at(-1);
+    if (
+        lastMessage &&
+        lastMessage instanceof AIMessage &&
+        lastMessage.tool_calls?.length
+    ) {
+        return "tools";
+    }
+    return "end";
+};
+
+const workflow = new StateGraph(MessagesAnnotation)
+    .addNode("llmCall", llmCall)
+    .addNode("tools", toolNode)
+    .addEdge("__start__", "llmCall")
+    .addConditionalEdges(
+        "llmCall",
+        shouldContinue,
+        {
+            tools: "tools",
+            end: "__end__"
+        }
+    )
+    .addEdge("tools", "llmCall");
+
+const app = workflow.compile();
+
+const messages = [{
+    role: "user",
+    content: "Add 12 + 18, then divide it from 5, and then multiply it by 6"
+}];
+
+const result = await app.invoke({ messages });
+console.log(result.messages);
