742c6aa1088e249fca128eef87eb536008ba5c3f
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-05-06 22:43:36 +0530

    feat: initialize weather MCP server with city lookup tool and dependency configuration

--- 2. Advance-GenAI/5. MCP/index.ts
+++ 2. Advance-GenAI/5. MCP/index.ts
@@ -1,1 +1,38 @@
-console.log("Hello via Bun!");
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
+import { z } from 'zod'
+
+const server = new McpServer({
+    name: "Weather MCP Server",
+    description: "Weather Data Fetcher MCP server",
+    version: "1.0.0",
+});
+
+async function getWeatherByCityName(city: string) {
+    if (city.toLowerCase() == 'varansi') {
+        return { temp: '30C', forecast: 'sunny' }
+    }
+    if (city.toLowerCase() == 'mumbai') {
+        return { temp: '26C', forecast: 'rainy' }
+    }
+    if (city.toLowerCase() == 'banglore') {
+        return { temp: '24C', forecast: 'cloudy' }
+    }
+    if (city.toLowerCase() == 'delhi') {
+        return { temp: '26C', forecast: 'rainy' }
+    }
+}
+
+server.tool(
+    'getWeatherByCityName',
+    {
+        city: z.string(),
+    },
+    async ({ city }) => {
+        return {
+            content: [{
+                type: "text",
+                text: JSON.stringify(getWeatherByCityName(city))
+            }]
+        }
+    }
+)
