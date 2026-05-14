b01cd750c26f0734ff8885aaf1d47832265da77b
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-05-14 21:03:11 +0530

    feat: initialize backend server with Prisma ORM and PostgreSQL database integration

--- 2. Advance-GenAI/7. AI-Chat-PDF-RAG/server/server.ts
+++ 2. Advance-GenAI/7. AI-Chat-PDF-RAG/server/server.ts
@@ -1,7 +1,14 @@
 import app from "./src/app";
+import { prisma } from "./db/connection";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`http://localhost:${PORT}`);
+    if (!process.env.DATABASE_URL) {
+        console.error("DATABASE_URL is not defined");
+        return;
+    }
+    await prisma.$connect();
+    console.log('Connected to database');
 });
