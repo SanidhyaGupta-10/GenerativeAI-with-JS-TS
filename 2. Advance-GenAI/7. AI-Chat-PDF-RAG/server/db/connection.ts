29d717ece8e7c0cb8a31564bd89ed64d72070509
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-05-14 21:59:06 +0530

    feat: initialize Prisma schema and database connection configuration

--- 2. Advance-GenAI/7. AI-Chat-PDF-RAG/server/db/connection.ts
+++ 2. Advance-GenAI/7. AI-Chat-PDF-RAG/server/db/connection.ts
@@ -1,18 +1,22 @@
+import { config } from 'dotenv';
+config(); // load .env before anything else
+
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
-const pool = new Pool();
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 const adapter = new PrismaPg(pool);
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma =
+    globalForPrisma.prisma || new PrismaClient({ adapter });
 
 export async function ConnectDB() {
     if (!process.env.DATABASE_URL) {
-        console.error("DATABASE_URL is not defined");
+        console.error('DATABASE_URL is not defined');
         return;
     }
     try {
