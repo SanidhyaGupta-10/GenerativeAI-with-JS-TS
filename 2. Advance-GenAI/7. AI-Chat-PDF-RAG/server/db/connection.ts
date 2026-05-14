46c705d1977f530ffb3189ba7127eb8875331341
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-05-14 21:22:10 +0530

    feat: initialize Express server with Prisma database integration and user routing structure

--- 2. Advance-GenAI/7. AI-Chat-PDF-RAG/server/db/connection.ts
+++ 2. Advance-GenAI/7. AI-Chat-PDF-RAG/server/db/connection.ts
@@ -2,12 +2,23 @@ import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
+const pool = new Pool();
 
 const adapter = new PrismaPg(pool);
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+export async function ConnectDB() {
+    if (!process.env.DATABASE_URL) {
+        console.error("DATABASE_URL is not defined");
+        return;
+    }
+    try {
+        await prisma.$connect();
+        console.log('Connected to database');
+    } catch (error) {
+        console.error('Error connecting to database', error);
+    }
+}
--- 2. Advance-GenAI/7. AI-Chat-PDF-RAG/server/server.ts
+++ 2. Advance-GenAI/7. AI-Chat-PDF-RAG/server/server.ts
@@ -1,14 +1,9 @@
-import app from "./src/app";
-import { prisma } from "./db/connection";
+import app from "./app";
+import { ConnectDB } from "./db/connection";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
     console.log(`http://localhost:${PORT}`);
-    if (!process.env.DATABASE_URL) {
-        console.error("DATABASE_URL is not defined");
-        return;
-    }
-    await prisma.$connect();
-    console.log('Connected to database');
+    await ConnectDB();
 });
