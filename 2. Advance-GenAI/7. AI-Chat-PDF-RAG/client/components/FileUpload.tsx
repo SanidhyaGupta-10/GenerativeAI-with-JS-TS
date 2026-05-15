d93c1e7910c8c6aaf94410e8b0c79b167afa0c22
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-05-15 13:48:28 +0530

    feat: implement FileUpload component and initialize backend project dependencies

--- 2. Advance-GenAI/7. AI-Chat-PDF-RAG/client/components/FileUpload.tsx
+++ 2. Advance-GenAI/7. AI-Chat-PDF-RAG/client/components/FileUpload.tsx
@@ -1,8 +1,22 @@
 "use client";
 import { Upload } from "lucide-react";
 
-
 function FileUpload() {
+    // This will create a file input element and click it
+    const handleFileUpload = () => {
+        const el = document.createElement("input")
+        el.setAttribute("type", "file")
+        el.setAttribute("accept", ".pdf")
+        el.click()
+        el.addEventListener("change", (e: Event) => {
+            const target = e.target as HTMLInputElement
+            const file = target.files?.[0]
+            if (file) {
+                console.log(file)
+            }
+        })
+    }
+
     return (
         <div className="glass-card h-[80vh] p-6 flex flex-col items-center justify-center ">
             <div className="mb-4 text-center">
@@ -12,12 +26,11 @@ function FileUpload() {
             <div className="space-y-4 w-full max-w-md">
                 <div className="space-y-2">
                     <label className="block text-sm font-medium text-white/80">PDF File</label>
-                    <div className="border-2 border-white/10 rounded-xl p-4 bg-white/5 hover:border-violet-500 transition-colors cursor-pointer flex flex-col items-center justify-center h-32">
+                    <div onClick={handleFileUpload} className="border-2 border-white/10 rounded-xl p-4 bg-white/5 hover:border-violet-500 transition-colors cursor-pointer flex flex-col items-center justify-center h-32">
                         <Upload className="w-10 h-10 text-violet-500 mb-2" />
                         <span className="text-white/80 text-sm">Click to upload or drag and drop</span>
                         <span className="text-white/60 text-xs mt-1">PDF files only</span>
                     </div>
-                    <input type="file" accept=".pdf" className="hidden" />
                 </div>
             </div>
         </div>
